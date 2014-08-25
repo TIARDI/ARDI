ecf2efd92107fd0498e76f64386fd026839bc07e
Rasmus Bækgaard <git@bakgaard.net>
2014-08-25 11:56:11 +0200

    swap content of .cpp and .hpp

--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/INET_Addr.cpp
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/INET_Addr.cpp
@@ -1,11 +1,37 @@
 #include "INET_Addr.hpp"
 
-class INET_Addr
+class INET_Addr 
 {
 public:
-	INET_Addr (uint16_t port, uint64_t addr);
-	uint16_t get_port () const ;
-	uint64_t get_ip_addr () const;
-	const sockaddr *addr () const;
-	size_t size () const;
+	INET_Addr::INET_Addr (uint16_t port, uint64_t addr) 
+	{
+		// Set up the address to become a server.
+		memset (&addr_, 0, sizeof addr_);
+		addr_.sin_family = AF_INET;
+		addr_.sin_port = htons (port);
+		addr_.sin_addr.s_addr = htonl (addr);
+	}
+
+	uint16_t INET_Addr::get_port () const 
+	{ 
+		return addr_.sin_port; 
+	}
+
+	uint64_t INET_Addr::get_ip_addr () const
+	{ 
+		return addr_.sin_addr.s_addr; 
+	}
+	
+	const sockaddr INET_Addr::*addr () const
+	{ 
+		return reinterpret_cast <sockaddr *>(&addr_);
+	}
+
+	size_t INET_Addr::size() const 
+	{ 
+		return sizeof (addr_);
+	}
+	// ...
+private:
+	sockaddr_in addr_;
 };
--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/INET_Addr.hpp
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/INET_Addr.hpp
@@ -1,37 +1,12 @@
 #include <stdint.h>
 #include <WINSOCK.h>
 
-class INET_Addr 
+class INET_Addr
 {
 public:
-	INET_Addr (uint16_t port, uint64_t addr) 
-	{
-		// Set up the address to become a server.
-		memset (&addr_, 0, sizeof addr_);
-		addr_.sin_family = AF_INET;
-		addr_.sin_port = htons (port);
-		addr_.sin_addr.s_addr = htonl (addr);
-	}
-	uint16_t get_port () const 
-	{ 
-		return addr_.sin_port; 
-	}
-
-	uint64_t get_ip_addr () const
-	{ 
-		return addr_.sin_addr.s_addr; 
-	}
-	
-	const sockaddr *addr () const
-	{ 
-		return reinterpret_cast <sockaddr *>(&addr_);
-	}
-
-	size_t size () const 
-	{ 
-		return sizeof (addr_);
-	}
-	// ...
-private:
-	sockaddr_in addr_;
+	INET_Addr(uint16_t port, uint64_t addr);
+	uint16_t get_port() const ;
+	uint64_t get_ip_addr() const;
+	const sockaddr *addr() const;
+	size_t size() const;
 };
