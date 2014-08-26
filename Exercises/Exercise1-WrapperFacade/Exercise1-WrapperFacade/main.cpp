c52b5ee66bdeacf1ff2929d60008ccb87d37c438
ChristofferW <thechocobo@gmail.com>
2014-08-26 11:02:10 +0200

    main in proj

--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/main.cpp
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/main.cpp
@@ -34,51 +34,22 @@ int main(int argc, char *argv[])
 void RunClient()
 {
 	
-	INET_Addr addr
-	struct sockaddr_in sock_addr;
-	// Handle UNIX/Win32 portability.
 
-
-	#if defined (_WIN32)
-		SOCKET acceptor;
-		WORD version_requested = MAKEWORD(2,0);
-		WSADATA wsa_data;
-		int error = WSAStartup(version_requested, &wsa_data);
-		if (error != 0) return -1;
-	#else
-		int acceptor;
-	#endif /* _WIN32 */
-	// Create a local endpoint of communication.
-	acceptor = socket (AF_INET,SOCK_STREAM, 0);
-	// Set up the address to become a server.
-	memset (&sock_addr, 0, sizeof(sock_addr));
-	sock_addr.sin_family = PF_INET;
-	sock_addr.sin_port = htons(LOGGING_PORT);
-	sock_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	// Associate address with endpoint.
-	bind (acceptor,reinterpret_cast<struct sockaddr *>(&sock_addr), sizeof(sock_addr));
-	// Make endpoint listen for connections.
-	listen (acceptor, 5);
-
-		while(true) 
-		{
-		// Handle UNIX/Win32 portability.
-		#if defined (_WIN32)
-			SOCKET h;
-			DWORD t_id;
-		#else
-			int h;
-			thread_t t_id;
-		#endif /* _WIN32 */
-		// Block waiting for clients to connect.
-		h = accept (acceptor, 0, 0);
-		//Spawn a new thread that runs the <server> entry point.
-		#if defined (_WIN32)
-			CreateThread (0, 0,LPTHREAD_START_ROUTINE(&logging_handler),reinterpret_cast <void *>(h), 0, &t_id);
-		#else
-			thr_create(0, 0, logging_handler,reinterpret_cast <void *> (h),THR_DETACHED, &t_id);
-		#endif /* _WIN32 */
-		}
 }
 
-void RunServer();
+void RunServer()
+{
+
+	struct sockaddr_in sock_addr;
+	INET_Addr inet(2222,INADDR_ANY);
+	SOCK_Acceptor acceptor(inet);
+	
+	SOCK_Stream stream;
+
+	while(true) 
+	{
+		acceptor.accept (stream);
+
+		//receive from
+	}
+}
