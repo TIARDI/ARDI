a2610c99a5ef18ccda728eb79c84d8df0413e0cf
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-08-26 11:51:40 +0200

    Error handling added to SOCK_Connector.

--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_Connector.cpp
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_Connector.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "SOCK_Connector.hpp"
 #include "winsockHandling.hpp"
+#include <stdexcept>
 
 
 SOCK_Connector::SOCK_Connector()
@@ -13,7 +14,10 @@ void SOCK_Connector::connect(SOCK_Stream& server, INET_Address& addr)
 	SOCKET s = server.get_handle();
 	const sockaddr* ad = addr.addr();
 
-	::connect(s, ad, addr.size());
+	if (::connect(s, ad, addr.size()) != 0)
+	{
+		throw std::runtime_error("Connect() failed.");
+	}
 }
 
 SOCK_Connector::~SOCK_Connector()
