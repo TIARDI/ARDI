dd3f43269fdf782bb3b2eaaa3fbd22c44fe428db
Rasmus Bækgaard <git@bakgaard.net>
2014-08-26 09:25:55 +0200

    Update SOCK Acceptor and Stream

--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_stream.h
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_stream.h
@@ -1,26 +1,21 @@
+#include <stddef.h>
 
 typedef int SOCKET;
-const int IN
+const int IN;
 
 class SOCK_Stream {
 public:
-// Default and copy constructor.
-	SOCK_Stream () : handle_
-	(INVALID_HANDLE_VALUE) { }
-	SOCK_Stream (SOCKET h): handle_ (h) { }
-// Automatically close the handle on destruction.
-	~SOCK_Stream () { close (handle_); }
-// Set/get the underlying SOCKET handle.
-	void set_handle (SOCKET h) { handle_ = h; }
-	SOCKET get_handle () const { return handle_; }
-// Regular I/O operations.
-	ssize_t recv (void *buf, size_t len, int flags);
-	ssize_t send (const char *buf, size_t len, int flags);
-// I/O operations for "short" receives and sends.
-	ssize_t recv_n (char *buf, size_t len, int flags);
-	ssize_t send_n (const char *buf, size_t len, int flags);
-// ... other methods omitted.
+	SOCK_Stream();
+	SOCK_Stream(SOCKET h);
+	~SOCK_Stream();
+	void set_handle(SOCKET h);
+	SOCKET get_handle() const;
+	size_t recv(void *buf, size_t len, int flags);
+	size_t send(const char *buf, size_t len, int flags);
+	size_t recv_n(char *buf, size_t len, int flags);
+	size_t send_n(const char *buf, size_t len, int flags);
+
 private:
-// Socket handle for exchanging socket data.
+	// Socket handle for exchanging socket data.
 	SOCKET handle_;
 };
