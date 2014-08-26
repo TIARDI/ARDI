7f8fa500700148be0c030301fde733ae9a853731
ChristofferW <thechocobo@gmail.com>
2014-08-26 11:40:31 +0200

    main

--- Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/main.cpp
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/main.cpp
@@ -6,23 +6,48 @@
 #include "SOCK_stream.h"
 #include "INET_Address.hpp"
 
-void RunClient();
-void RunServer();
+
+void RunClient()
+{
+	std::string message = "Hello from client";
+	SOCK_Stream stream;
+	SOCK_Connector connector;
+	INET_Addr addr(2222,inet_addr("127.0.0.1"));
+	connector.connect(stream,addr);
+	stream.send(message.c_str(), message.size(),0);
+}
+
+void RunServer()
+{
+	struct sockaddr_in sock_addr;
+	INET_Addr inet(2222,INADDR_ANY);
+	SOCK_Acceptor acceptor(inet);
+	
+	SOCK_Stream stream;
+
+	while(true) 
+	{
+		acceptor.accept (stream);
+
+		//receive from
+	}
+
+}
 
 int main(int argc, char *argv[])
 {
-	if(argc != 1)
+	if(argc != 2)
 	{
 		std::cout << "Only one argument is allowed" << std::endl;
 		return -1;
 	}
 
-	if(argv[0] == "c")
+	if(argv[1] == "c")
 	{
 		std::cout << "Running as client..." << std::endl;
 		RunClient();
 	}
-	else if(argv[0] == "s")
+	else if(argv[1] == "s")
 	{
 		std::cout << "Running as server..." << std::endl;
 		RunServer();
@@ -35,24 +60,4 @@ int main(int argc, char *argv[])
 
 }
 
-void RunClient()
-{
-	
 
-}
-
-void RunServer()
-{
-	struct sockaddr_in sock_addr;
-	INET_Address inet(2222,INADDR_ANY);
-	SOCK_Acceptor acceptor(inet);
-	
-	SOCK_Stream stream;
-
-	while(true) 
-	{
-		acceptor.accept (stream);
-
-		//receive from
-	}
-}
