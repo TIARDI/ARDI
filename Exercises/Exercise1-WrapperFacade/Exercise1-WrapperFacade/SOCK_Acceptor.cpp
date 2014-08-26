65edb435ebb5c54dd69d77cd5b46452c023c89fc
Martin Manø <mail@madsprayer.dk>
2014-08-26 08:40:43 +0200

    ADD - SOCK_Accepter

---
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_Acceptor.cpp
@@ -0,0 +1,23 @@
+#include "SOCK_Acceptor.hpp"
+
+SOCK_Acceptor::SOCK_Acceptor (const INET_Addr &addr)
+{
+	// Create a local endpoint of communication.
+	handle_ = socket (PF_INET, SOCK_STREAM, 0);
+	// Associate address with endpoint.
+	bind (handle_, addr.addr (), addr.size());
+	// Make endpoint listen for connections.
+	listen (handle_, 5);
+};
+// A second method to initialize a passivemode
+// acceptor socket, analogously to the constructor.
+void SOCK_Acceptor::open (const INET_Addr &sock_addr)
+{
+	// Implement Code
+};
+
+// Accept a connection and initialize the <stream>.
+void SOCK_Acceptor::accept (SOCK_Stream &s)
+{
+	s.set_handle (accept (handle_, 0, 0));
+}
---
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_Acceptor.hpp
@@ -0,0 +1,20 @@
+#include <winsock.h>
+#include "INET_Addr.hpp"
+#include "SOCK_stream.h"
+
+class SOCK_Acceptor
+{
+public:
+	// Initialize a passive-mode acceptor socket.
+	SOCK_Acceptor (const INET_Addr &addr);
+
+	// A second method to initialize a passivemode
+	// acceptor socket, analogously to the constructor.
+	void open (const INET_Addr &sock_addr);
+
+	// Accept a connection and initialize the <stream>.
+	void accept (SOCK_Stream &s);
+
+private:
+	SOCKET handle_; // Socket handle factory.
+};
