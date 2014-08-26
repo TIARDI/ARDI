fdf61fc706dab4b21e62513d87a1623c7a27e0a7
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-08-26 09:28:23 +0200

    SOCK_Connector initial commit.

---
+++ Exercises/Exercise1-WrapperFacade/Exercise1-WrapperFacade/SOCK_Connector.cpp
@@ -0,0 +1,17 @@
+#include "SOCK_Connector.hpp"
+
+SOCK_Connector::SOCK_Connector()
+{
+}
+
+void SOCK_Connector::connect(SOCK_Stream& server, INET_Addr& addr)
+{
+	SOCKET s = server.get_handle();
+	const sockaddr* ad = addr.addr();
+
+	::connect(s, ad, addr.size());
+}
+
+SOCK_Connector::~SOCK_Connector()
+{
+}
