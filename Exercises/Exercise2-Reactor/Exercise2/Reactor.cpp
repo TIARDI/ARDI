ddccd327e33d30a0e47c267b835cd2fc76347666
Rasmus Bækgaard <git@bakgaard.net>
2014-09-01 11:18:54 +0200

    Add Reactor. Can't see the Event_handler.h and throws errors

---
+++ Exercises/Exercise2-Reactor/Exercise2/Reactor.cpp
@@ -0,0 +1,41 @@
+#include "Reactor.h"
+#include "Event_handler.h"
+
+#ifdef WIN32
+	#include <WinSock2.h>
+#else
+
+#endif
+
+void Reactor::register_handler(Event_Handler *eh, Event_Type et)
+{
+
+}
+
+void Reactor::register_handler(HANDLE h, Event_Handler *eh, Event_Type et) 
+{
+
+}
+
+void Reactor::remove_handler(Event_Handler *eh, Event_Type et)
+{
+
+}
+
+void Reactor::remove_handler(HANDLE h, Event_Type et) const
+{
+
+}
+
+// Entry point into the reactive event loop
+void Reactor::handle_events(timeval *timeout = 0)
+{
+
+}
+
+// Define a singleton access point (GoF pattern)
+static Reactor* Reactor::instance()
+{
+	return reactor_impl_;
+
+}
---
+++ Exercises/Exercise2-Reactor/Exercise2/Reactor.h
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+
+class Reactor
+{
+public:
+	virtual void register_handler(Event_Handler *eh, Event_Type et) = 0;
+	virtual void register_handler(HANDLE h, Event_Handler *eh, Event_Type et) = 0;
+	virtual void remove_handler(Event_Handler *eh, Event_Type et) = 0;
+	virtual void remove_handler(HANDLE h, Event_Type et) const = 0;
+	
+	// Entry point into the reactive event loop
+	void handle_events(timeval *timeout =0);
+	
+	// Define a singleton access point (GoF pattern)
+	static Reactor *instance();
+private:
+	Reactor_Implementation *reactor_impl_; // uses the GoF Bridge pattern
+};
