69a7ccf1efadd1d571d41f46184329f5aa2fceca
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-09-01 11:24:38 +0200

    Event_handler header added.

---
+++ Exercises/Exercise2-Reactor/Exercise2/Event_handler.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <winsock2.h>
+
+enum Event_type
+{
+	READ = 0x01,
+	ACCEPT = READ,
+	WRITE = 0x02,
+	TIMEOUT = 0x03
+};
+
+class Event_Handler
+{
+public:
+	virtual void handle_event(HANDLE h, Event_type eType) = 0;
+	virtual HANDLE get_handle() const = 0;
+
+	virtual ~Event_Handler() = 0;
+};
