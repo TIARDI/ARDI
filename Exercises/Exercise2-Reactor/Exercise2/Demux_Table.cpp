bf22f94905a54c8f6c05d2a7eb1406522a1a1e7e
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-09-01 12:58:16 +0200

    demux_table added.

---
+++ Exercises/Exercise2-Reactor/Exercise2/Demux_Table.cpp
@@ -0,0 +1,31 @@
+#include <stdexcept>
+#include "stdafx.h"
+#include "Demux_Table.h"
+
+
+void Demux_Table::convert_to_fd_sets(fd_set *read_fds, fd_set *write_fds, fd_set *except_fds)
+{
+	FD_ZERO(read_fds);
+	FD_ZERO(write_fds);
+	FD_ZERO(except_fds);
+
+	for (const auto handleKvp : (*this))
+	{
+		if (handleKvp.second.type == READ || handleKvp.second.type == ACCEPT)
+		{
+			FD_SET((SOCKET)handleKvp.first, read_fds);
+		}
+		else if (handleKvp.second.type == WRITE)
+		{
+			FD_SET((SOCKET)handleKvp.first, write_fds);
+		}
+		else if (handleKvp.second.type == EXCEPT)
+		{
+			FD_SET((SOCKET)handleKvp.first, except_fds);
+		}
+		else
+		{
+			throw std::invalid_argument("Event_type not supported.");
+		}
+	}
+}
--- Exercises/Exercise2-Reactor/Exercise2/Event_handler.hpp
+++ Exercises/Exercise2-Reactor/Exercise2/Event_handler.hpp
@@ -6,7 +6,8 @@ enum Event_type
 	READ = 0x01,
 	ACCEPT = READ,
 	WRITE = 0x02,
-	TIMEOUT = 0x03
+	TIMEOUT = 0x03,
+	EXCEPT = 0x04
 };
 
 class Event_Handler
