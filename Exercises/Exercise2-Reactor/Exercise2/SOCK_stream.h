55f667e1bebce531e1167ad54ef31a4ef3033159
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-09-08 11:41:22 +0200

    commented out unused functions.

--- Exercises/Exercise2-Reactor/Exercise2/SOCK_stream.h
+++ Exercises/Exercise2-Reactor/Exercise2/SOCK_stream.h
@@ -18,8 +18,8 @@ public:
 	SOCKET get_handle() const;
 	size_t recv(char *buf, size_t len, int flags);
 	size_t send(const char *buf, size_t len, int flags);
-	size_t recv_n(char *buf, size_t len, int flags);
-	size_t send_n(const char *buf, size_t len, int flags);
+	//size_t recv_n(char *buf, size_t len, int flags);
+	//size_t send_n(const char *buf, size_t len, int flags);
 
 private:
 	// Socket handle for exchanging socket data.
