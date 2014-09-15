b8038b4b05b37037f6ce3afddac77f885675c96e
Lasse Brøsted Pedersen <lasse@huhej.dk>
2014-09-15 10:49:46 +0200

    Read message in eventhandler

--- Exercises/Exercise2-Reactor/Exercise2/Alarm_EventHandler.cpp
+++ Exercises/Exercise2-Reactor/Exercise2/Alarm_EventHandler.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include "Alarm_EventHandler.h"
 #include <iostream>
+#include <iomanip>
 #include <ctime>
+#include <array>
 
 Alarm_EventHandler::Alarm_EventHandler(std::shared_ptr<SOCK_Stream>stream, Reactor *reactor)
 	: _peer_stream(stream), _reactor(reactor)
@@ -17,35 +19,28 @@ Alarm_EventHandler::~Alarm_EventHandler()
 
 void Alarm_EventHandler::handle_event(HANDLE h, Event_type eType)
 {
-	char* rxValue = new char[];
-	char* size = new char[];
-	int res = _peer_stream->recv(size,sizeof(int),0);
-
-	if(res == 0)
-	{
-		
-		delete rxValue;
-		delete size;
-
-		_reactor->remove_handler(this, eType);
-		return;
-	}
 
 	if(eType == READ)
 	{
+		std::array<char, 100> buffer;
+		buffer.fill(0);
+
+		auto res = _peer_stream->recv(buffer.data(), buffer.size(), 0);
+
+		if (res == 0)
+		{
+			std::cout << "Alarm_EventHandler: connection closed." << std::endl;
+			_reactor->remove_handler(this, eType);
+			return;
+		}
+
 		time_t t = time(0);
-		struct tm timeInfo; 
+		std::tm timeInfo; 
 		localtime_s(&timeInfo, &t);
 		
-		std::cout << "Current time is: " 
-		<< timeInfo.tm_hour << ":"
-		<< timeInfo.tm_min << ":"
-		<< timeInfo.tm_sec
-		<< " on the "
-		<< timeInfo.tm_mday << "/"
-		<< timeInfo.tm_mon  + 1 << "-"
-		<< timeInfo.tm_year + 1900 
-		<< std::endl;
+		std::cout << "Current time is: " << std::put_time(&timeInfo, "%c") << std::endl;
+		std::cout << "Priority: " << buffer[0] << std::endl;
+		std::cout << "Message: " << buffer.data() + 1 << std::endl;
 	}
 	else
 	{
