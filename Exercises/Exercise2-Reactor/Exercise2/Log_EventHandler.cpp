7b9e27e7fc5dba3a8be9f24f68435fe5ff38b29d
ChristofferW <thechocobo@gmail.com>
2014-09-15 11:01:43 +0200

    log event handler changed to smart pointer

--- Exercises/Exercise2-Reactor/Exercise2/Log_EventHandler.cpp
+++ Exercises/Exercise2-Reactor/Exercise2/Log_EventHandler.cpp
@@ -2,7 +2,7 @@
 #include "stdafx.h"
 #include "Log_EventHandler.h"
 
-Log_EventHandler::Log_EventHandler(SOCK_Stream& stream, Reactor* reactor)
+Log_EventHandler::Log_EventHandler(std::shared_ptr<SOCK_Stream> stream, Reactor* reactor)
 	: _peer_stream(stream), _reactor(reactor)
 {
 	_reactor->register_handler(this, Event_type::READ);
@@ -15,15 +15,21 @@ Log_EventHandler::~Log_EventHandler()
 	
 void Log_EventHandler::handle_event(HANDLE h, Event_type eType)
 {
-		if(eType == READ)
+		char* rxValue = new char[];
+		int res = _peer_stream->recv(rxValue,100,0);
+
+		if(res == 0)
 		{
-			char* rxValue = new char[];
-			char* size = new char[];
-			int res = _peer_stream.recv(size,sizeof(int),0);
-			res = _peer_stream.recv(rxValue,atoi(size),0);
+			std::cout << "Connection closed... Closing handler" << std::endl;
+						_reactor->remove_handler(this,READ);
+			delete this;
+
+		}
+		else if(eType == READ)
+		{
+
 			std::cout << "Log message: " << std::string(rxValue) << std::endl;
 			delete rxValue;
-			delete size;
 		}
 		else
 		{
