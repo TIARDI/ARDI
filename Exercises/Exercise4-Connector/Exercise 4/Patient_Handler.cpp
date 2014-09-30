4a98971904ae765ed7b3af90f74a872a8ae9b2c9
Rasmus Bækgaard <git@bakgaard.net>
2014-09-30 10:33:33 +0200

    PatientHandler fix

--- Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.cpp
+++ Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.cpp
@@ -1,9 +1,6 @@
 #include "stdafx.h"
 #include "Patient_Handler.hpp"
 
-Patient_Handler::Patient_Handler()
-{}
-
 void Patient_Handler::handle_event(HANDLE h, Event_type eType)
 {
 
--- Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.hpp
+++ Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.hpp
@@ -3,7 +3,7 @@
 #include <WinSock2.h>
 #include "Service_Handler.hpp"
 
-class Patient_Handler : public Service_Handler<SOCK_Stream>
+class Patient_Handler : public Service_Handler
 {
 	//Patient_Handlers
 	void handle_event(HANDLE h, Event_type eType);
