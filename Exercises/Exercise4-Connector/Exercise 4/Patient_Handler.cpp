dd3614f2cf7cb855921f357fc8e5848c077adb7b
ChristofferW <thechocobo@gmail.com>
2014-09-30 11:36:12 +0200

    stream is protected, and added implementation of patient_handler

--- Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.cpp
+++ Exercises/Exercise4-Connector/Exercise 4/Patient_Handler.cpp
@@ -8,7 +8,17 @@ void Patient_Handler::open()
 
 void Patient_Handler::handle_event(HANDLE h, Event_type eType)
 {
-	//Get data here
+	char buffer[100];
+	ipc_stream_.recv(buffer,100,0);
+	std::string val(buffer);
+	if(val == "111111-1111")
+		ipc_stream_.send("Rasmus,Bækgaard,AIDS",20,0);
+	else if(val == "222222-2222")
+		ipc_stream_.send("Lasse,Pedersen,Chlamydia",23,0);
+	else if(val == "333333-3333")
+		ipc_stream_.send("Martin,Manø,Gonorrhea",21,0);
+	else if(val == "444444-4444")
+		ipc_stream_.send("Christoffer,Werge,Herpes",24,0);
 }
 
 Patient_Handler::~Patient_Handler()
