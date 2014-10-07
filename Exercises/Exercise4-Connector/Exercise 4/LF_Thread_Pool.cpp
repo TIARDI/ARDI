26f6d7e3afcc4eb46a627da0e5a9574637790987
Martin Manø <mail@madsprayer.dk>
2014-10-07 11:47:46 +0200

    ADD - Files for Thead Pool

---
+++ Exercises/Exercise4-Connector/Exercise 4/LF_Thread_Pool.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "LF_Thread_Pool.h"
+
+
+void LF_Thread_Pool::join(int timeout)
+{
+	mutex_.lock();
+
+	for (;;)
+	{
+		while (leader_thread_ == std::thread::id())
+		{
+			followers_condition_.wait_for(std::unique_lock<std::mutex>(mutex_), std::chrono::seconds(timeout));
+		}
+
+		leader_thread_ = std::this_thread::get_id();
+
+		mutex_.unlock();
+
+		reactor_->handle_events();
+
+		mutex_.lock();
+	}
+	mutex_.unlock();
+}
+
+void LF_Thread_Pool::promote_new_leader()
+{
+	mutex_.lock();
+
+	if (leader_thread_ != std::this_thread::get_id())
+		throw;
+
+	leader_thread_ = std::thread::id();
+
+	followers_condition_.notify_all();
+
+	mutex_.unlock();
+}
+
+void LF_Thread_Pool::deactivate_handle(HANDLE handle, Event_type eType)
+{
+
+}
+
+void LF_Thread_Pool::reactivate_handler(HANDLE handle, Event_type eType)
+{
+
+}
